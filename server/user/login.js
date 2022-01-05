7e0f7bf4687ec11bceb45f6626ac1f3a251e459e
reklesswang <reklesswang@tencent.com>
2022-01-05 15:09:03 +0800

    load user config every time

--- server/user/login.js
+++ server/user/login.js
@@ -4,12 +4,15 @@ const utils = require('kernel/utils');
 const config = require('kernel/config');
 const jwt = require('jsonwebtoken');
 const auth = require('kernel/auth').create({jwtSecret: config.jwtSecret, jwt})
-const accessUser = require('./load_user_config').create(config.localUserConfigPath).readUserFromFile();
 
 exports.main_handler = async (ctx) => {
   const q = utils.parseKoaRequest(ctx);
   if (!q.username) throw errors.create(errors.SystemVerifyError, `username required`);
   if (!q.password) throw errors.create(errors.SystemVerifyError, `password required, username=${q.username}`);
+
+  // load user every time
+  const accessUser = require('./load_user_config').create(config.localUserConfigPath).readUserFromFile();
+
   if (q.username !== accessUser.username) throw errors.create(errors.SystemVerifyError, `username=${q.username} invalid`);
   if (q.password !== accessUser.password) throw errors.create(errors.SystemVerifyError, `password invalid`);
 
