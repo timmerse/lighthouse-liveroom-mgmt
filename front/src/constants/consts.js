a19c8eb71a75bda511ff0c978976b3725c168f09
reklesswang <reklesswang@tencent.com>
2022-01-06 14:17:38 +0800

    fix error

--- front/src/constants/consts.js
+++ front/src/constants/consts.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.REACT_APP_SERVER_URL || `http://81.70.144.106:9998/base/v1`;
+export const API_BASE_URL = process.env.REACT_APP_SERVER_URL || `http://${window.location.hostname}:9998/base/v1`;
 export const LOGIN_ACCESS_TOKEN = 'login_access_token';
 export const LOGIN_ACCESS_USERNAME = 'login_access_username';
