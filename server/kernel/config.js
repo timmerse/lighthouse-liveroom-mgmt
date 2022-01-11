37bab8067dbeb11a008eba39f6bd348a247431b9
reklesswang <91418940+reklesswang@users.noreply.github.com>
2022-01-11 14:55:21 +0800

    update LOCAL_CONFIG_PATH

--- server/kernel/config.js
+++ server/kernel/config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   jwtSecret: process.env.JWT_SECRET || '12345',
-  localConfigPath: process.env.LOCAL_CONFIG_PATH || '/usr/local/lighthouse/softwares/liveroom/conf/env',
+  localConfigPath: process.env.LOCAL_CONFIG_PATH || '/usr/local/lighthouse/softwares/lighthouse-liveroom/conf/env',
   localUserConfigPath: process.env.USER_CONFIG_PATH || '/usr/local/lighthouse/softwares/lighthouse-liveroom-mgmt/conf/user.json'
 }
